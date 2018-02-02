3a5fe76b37760aabca6bcf229e836b648a9f0fea
Olegs Bogdanovs <bogdanovsolegs@gmail.com>
2018-02-02 11:08:40 +0200

    Initial commit

---
+++ src/main/java/lv/tsi/olegsbogdanovs/hardshop/HardshopApplication.java
@@ -0,0 +1,12 @@
+package lv.tsi.olegsbogdanovs.hardshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardshopApplication.class, args);
+	}
+}
