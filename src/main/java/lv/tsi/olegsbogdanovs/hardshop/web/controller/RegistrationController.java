10f1ef2c4f3b71b59c615507dbf1ac39b9fb0fa8
Olegs Bogdanovs <bogdanovsolegs@gmail.com>
2018-04-11 10:36:29 +0300

    user_add and user_update

--- src/main/java/lv/tsi/olegsbogdanovs/hardshop/web/controller/RegistrationController.java
+++ src/main/java/lv/tsi/olegsbogdanovs/hardshop/web/controller/RegistrationController.java
@@ -5,7 +5,6 @@ import lv.tsi.olegsbogdanovs.hardshop.persistanse.domain.User;
 import lv.tsi.olegsbogdanovs.hardshop.service.UserService;
 import lv.tsi.olegsbogdanovs.hardshop.validation.EmailExistsException;
 import lv.tsi.olegsbogdanovs.hardshop.web.dto.UserDto;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
