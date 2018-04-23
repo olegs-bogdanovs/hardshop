5eed17923f6f262168f7ead9bcb3d986150e7c45
Olegs Bogdanovs <bogdanovsolegs@gmail.com>
2018-04-23 15:16:13 +0300

    Add Use Case Diagrams. WebJar resource updated.

--- src/main/java/lv/tsi/olegsbogdanovs/hardshop/config/WebConfig.java
+++ src/main/java/lv/tsi/olegsbogdanovs/hardshop/config/WebConfig.java
@@ -11,8 +11,9 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/webjars/**")
-                .addResourceLocations("/webjars/");
+        if (!registry.hasMappingForPattern("/webjars/**")) {
+            registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        }
+        registry.setOrder(1);
     }
 }
